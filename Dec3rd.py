b9fd5d7991b931b0f1d109ec9e76bc79a96ee69b
Liam Stregger <liamstregger@gmail.com>
2023-12-03 19:55:21 -0800

    fix mistake

--- Dec3rd.py
+++ Dec3rd.py
@@ -1,4 +1,4 @@
-file = open('dec3.txt', 'r')
+file = open('sample.txt', 'r')
 data = file.read()
 file.close()
 temp = data.split('\n')
@@ -70,6 +70,46 @@ for x, row in enumerate(engine):
 numbs = [[int(j) for j in i] for i in numbs]
 print(f'part 1:{sum([sum(i) for i in numbs])}')
 ratios = 0
+
+# part 2
+numbs = []
+for x, row in enumerate(engine):
+    for y, i in enumerate(row):
+
+        if i == '*':   #<- change from part 1
+            numbs.append([])
+            n = findnum(x, y + 1)
+            if n != '0':
+                numbs[-1].append(n)
+            n = findnum(x, y - 1)
+            if n != '0':
+                numbs[-1].append(n)
+            n = findnum(x + 1, y)
+            if n == '0':
+                n = findnum(x + 1, y + 1)
+                if n != '0':
+                    numbs[-1].append(n)
+                n = findnum(x + 1, y - 1)
+                if n != '0':
+                    numbs[-1].append(n)
+            else:
+                numbs[-1].append(n)
+
+            n = findnum(x - 1, y)
+            if n == '0':
+                n = findnum(x - 1, y + 1)
+                if n != '0':
+                    numbs[-1].append(n)
+                n = findnum(x - 1, y - 1)
+                if n != '0':
+                    numbs[-1].append(n)
+            else:
+                numbs[-1].append(n)
+            if len(numbs[-1]) == 0:
+                numbs.pop(-1)
+numbs = [[int(j) for j in i] for i in numbs]
+
+#find the *s with 2 and multiply than add
 for i in numbs:
     if len(i) == 2:
         ratios += i[0] * i[1]
