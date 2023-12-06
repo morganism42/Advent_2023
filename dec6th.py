ffa4858227b38c97f92ed605b7fb865310d7cd57
morganism42 <liamstregger@gmail.com>
2023-12-05 21:53:42 -0800

    decided to compare naive to using the quadratic formula

--- dec6th.py
+++ dec6th.py
@@ -1,5 +1,6 @@
 from numpy import prod
 from time import perf_counter
+from math import sqrt,floor,ceil
 start = perf_counter()
 # get 2 lists for time and distancs
 with open('dec6.txt') as f:
@@ -15,5 +16,13 @@ for i in range(len(time)):  # iterate over each race
 		if j * (time[i] - j) > distance[i]:
 			wins.append([j, time[i] - j])
 			break  # math
+print(f'answer:{prod([(i[1] - i[0] + 1) for i in wins])}')
+print(f'took {perf_counter()-start}) seconds')
+start = perf_counter()
+wins = []
+for i in range(len(time)):
+	x = floor(-(-time[i]-sqrt(time[i]**2 - 4*distance[i]))/2)
+	y = ceil(-(-time[i] + sqrt(time[i] ** 2 - 4 * distance[i])) / 2)
+	wins.append([y,x])
 print(f'answer:{prod([(i[1] - i[0] + 1) for i in wins])}')
 print(f'took {perf_counter()-start}) seconds')
